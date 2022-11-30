fc560f9bf97eff2dcdff15ee47d007331b307890
kushal863 <kushalssaini14@gmail.com>
2022-11-30 12:42:31 +0530

    second_day_commit_first

---
+++ Days/Day-2/excercise.py
@@ -0,0 +1,27 @@
+#Data Types
+# String
+"Hello"
+print("Hello"[0])
+
+print("123" + "456")
+
+# Interger
+123
+print(123 + 345)
+
+print(123_5 + 563_3)
+
+# Float
+
+3.14159
+
+# Boolean
+True
+False
+
+# Data Type conversion
+num_char = len(input("what is your name?"))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " charecters.")
