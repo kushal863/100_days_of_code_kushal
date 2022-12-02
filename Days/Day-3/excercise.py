9e879c061774f61e6a69e3e1473a7fd96a5baecd
kushal863 <kushalskumar14@gmail.com>
2022-12-02 12:32:16 +0530

    Day-3 three challenge completed

---
+++ Days/Day-3/excercise.py
@@ -0,0 +1,20 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+# Check number is even or odd
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if (number%2==0):
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+
+#Write your code below this line 👇
+
