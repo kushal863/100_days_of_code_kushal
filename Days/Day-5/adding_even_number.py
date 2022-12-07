c66ef4e1296ecdbbd2a3b346eb3bc4fc7557d6da
kushal863 <kushalskumar14@gmail.com>
2022-12-07 09:53:41 +0530

    day-5-third-excercies

---
+++ Days/Day-5/adding_even_number.py
@@ -0,0 +1,13 @@
+# Going to add even numbers
+
+even_total =0
+for number in range(2,101,2):
+    if (number%2==0):
+        even_total+=number
+print(even_total)
+
+# Another Approach
+even_total =0
+for number in range(2,101,2):
+        even_total+=number
+print(even_total)
