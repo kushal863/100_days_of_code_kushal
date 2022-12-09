e86f67eb451f2726eeb448dffd898851b81afd30
kushal863 <kushalskumar14@gmail.com>
2022-12-09 11:37:49 +0530

    for-loop-while-loop

--- Days/Day-6/excercise.py
+++ Days/Day-6/excercise.py
@@ -8,3 +8,17 @@
 # calling function
 
 # my_function() # just call that function
+
+# for loop
+# when you want to do something with items
+
+# While
+# when you perform some type of functionality many many times then use while loop
+
+
+# While loop is little bit dangrous
+
+# in case of for loop you know the end( mean stop when reaches to the end)
+
+# on the other end while continues running if the condition is true
+# take atmost care while using while loop
