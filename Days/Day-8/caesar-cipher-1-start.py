963d620d1b96ec7e0e0ac63e3b6d438bf43c11da
kushal863 <kushalskumar14@gmail.com>
2022-12-19 12:34:58 +0530

    caesar_cipher

---
+++ Days/Day-8/caesar-cipher-1-start.py
@@ -0,0 +1,5 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+ 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrpt, type 'decode' to decrpt:\n")
+text = input("Type your message:\n")
