325c3aa68fb1a4645b3c9eb575ef1e4c41f4e033
kushal863 <kushalskumar14@gmail.com>
2022-12-21 12:53:26 +0530

    Day-2 completed

--- Days/Day-8/caesar-cipher_2.py
+++ Days/Day-8/caesar-cipher_2.py
@@ -19,11 +19,23 @@ def encrypt(message,shift_amount):
         letter_index=alphabet.index(letter)
         letter_index_shifted =letter_index+shift_amount
         encrypt_message+=alphabet[letter_index_shifted]
-    print(f"The encoded text is {encrypt_message}")
+    print(f"The encoded text is {encrypt_message}") 
 
-                
+encrypt(message=text,shift_amount=shift)
 
 
-  
+# task 2- Create a function decrypt that takes the text and shift backword as input
 
-encrypt(text,shift)
+def decrypt(message,shift_amount):
+    decrypt_message=''
+    for letter in message:
+        letter_index = alphabet.index(letter)
+        letter_index_shifted =letter_index-shift_amount
+        decrypt_message+=alphabet[letter_index_shifted]
+    
+    print(f"The decoded text is {decrypt_message}")
+
+if direction=='endode':
+    encrypt(message=text,shift_amount=shift)
+elif direction=='decode':
+    decrypt(message=text, shift_amount=shift)
