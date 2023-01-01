45952ba1834e2e394edefed3aaa71ce74bf072d2
kushal863 <kushalskumar14@gmail.com>
2023-01-01 12:33:59 +0530

    2023_first_commit

---
+++ Days/Day-9/main.py
@@ -0,0 +1,12 @@
+programming_dictionary ={
+    'Bug':'An error in a program that prevents the porgram from running as expected.',
+    'Function':'A piece of code that you can easily call over and over again'
+}
+
+
+
+# Adding new item in dictionary
+
+programming_dictionary['loop']='code block to run again and again'
+
+print(programming_dictionary)
