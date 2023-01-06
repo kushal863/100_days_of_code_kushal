7b2dac4fef9de277c0538367be365b93f58f2721
kushal863 <kushalskumar14@gmail.com>
2023-01-06 12:44:12 +0530

    second_commit

--- Days/Day-9/start_day-1.py
+++ Days/Day-9/start_day-1.py
@@ -6,17 +6,22 @@ capitals ={
 
 
 # Nested a list in dictionary
-trevel_blog_ ={
+trevel_blog={
     "France" : ["Paris","Little","Dijon"],
     "Germany":["Berlin","Hamburg","Stuttgart"]
 }
 
 
 # Nested dictionary in dictionary
-trevel_log ={
-
-    "France":{"cities_visited":["Paris","Lille","Dijon"],
-    "Total_visits":12}
+trevel_log={
+    "France":{"cities_visited":["Paris","Lille","Dijon"]},
+    "Total_visits":12
  }
 
 print(trevel_log["France"]["cities_visited"])
+
+trevel_log_my ={
+
+    "germany":{"cities,_visited":["Berlin","Hamburg","Stuttgart"]},
+    "Total_visits":12
+ }
