92f092649d28eca5ae952b5c3c1bc9fcd3f05d86
kushal863 <kushalskumar14@gmail.com>
2023-01-09 12:37:54 +0530

    excercise

--- Days/Day-9/dictionary_in_list.py
+++ Days/Day-9/dictionary_in_list.py
@@ -1,4 +1,51 @@
-# Nested Dictionary inside list
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(country_name,num_of_visits,cities_names):
+    int_dict = {
+        "country":country_name,
+        "visits":num_of_visits,
+        "cities": cities_names
+    }
+
+    travel_log.append(int_dict)
+    
+
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 trevel_blog_log =[
     {"country":"France",
