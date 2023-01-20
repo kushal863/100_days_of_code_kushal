b30e2dba1a19f9c636961e28c2887fd9715777e7
kushal863 <kushalskumar14@gmail.com>
2023-01-20 12:40:26 +0530

    done

--- Days/Day-13/main.py
+++ Days/Day-13/main.py
@@ -13,17 +13,17 @@
 # dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
 
-# Play Computer
-year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
-  print("You are a millenial.")
-elif year >= 1994:
-  print("You are a Gen Z.")
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
 
-# # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# Fix the Errors
+age = int(input("How old are you?"))
+if age > 18:
+    print("You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
