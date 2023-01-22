89a47dcc9cd49041efe53cde3f2184e3127265c1
kushal863 <kushalskumar14@gmail.com>
2023-01-22 14:46:14 +0530

    debugging_day-13 completed.

--- Days/Day-13/excercise_debug.py
+++ Days/Day-13/excercise_debug.py
@@ -9,3 +9,45 @@
 #     print("This is an even number.")
 # else:
 #     print("This is an odd number.")
+
+
+# what is type error
+
+# When you are trying something with wrong type error
+
+
+# Bug: year = input("Which year do you want to check?")
+# Actually we are deviding sting with number
+# To resolve this we just have converted sting into integer
+
+# year = int(input("Which year do you want to check?"))
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+  
+
+# FizzBuzz code here.. it contains errors just need to check and fix it
+
+# Bugs:
+# 1: in the else block number mentioned in the list.. just removed sequre brakets to fix
+# 2: in each block of code if statement used.. just removed to fix this.used elif instead
+# 3: or operator used in first condition.. where are checking 3 and 5 both condition. need to user and there.
+
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
