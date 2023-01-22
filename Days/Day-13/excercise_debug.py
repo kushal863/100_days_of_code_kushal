3507920d5b2785623c384a3dc30be8f1ea0e6315
kushal863 <kushalskumar14@gmail.com>
2023-01-22 14:27:54 +0530

    debuging

---
+++ Days/Day-13/excercise_debug.py
@@ -0,0 +1,11 @@
+# Modify the code and fix the bug
+
+# number = int(input("Which number do you want to check ? "))
+
+# Bug here is we are taking assigment operator intead consitional argument
+# if number %2 =0: it should be if number %2 ==0:
+
+# if number %2 ==0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
