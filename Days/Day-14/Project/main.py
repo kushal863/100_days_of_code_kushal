3bcbd01224aad96a6ee941c80e2439f4b08cb46f
kushal863 <kushalskumar14@gmail.com>
2023-01-25 20:48:17 +0530

    day_14 completed

--- Days/Day-14/Project/main.py
+++ Days/Day-14/Project/main.py
@@ -52,29 +52,35 @@ def folower_count(data,name):
 # function to get two stars data using a for first and b for b
 
 def generate_two_stars(data):
+    """
+    This function will return dictionary of two ( name and their follower count)
+    
+    """
     answer =['A','B']
     final_dict={}
 
+
     for i in range(2):
-        global random_name
+        # global random_name
         random_name = generate_random_name(data)
         final_dict[answer[i]]={random_name:folower_count(data,random_name)}
     return final_dict
-final=generate_two_stars(data)
 
-def compare(data,user_input):
+
+def compare(user_input,final):
+    """
+    This function compare values if a and b and return boolean value
+    """
     win=True
-    final = generate_two_stars(data)
-    
     if user_input =='A':
-        print(f"A values {list(final['A'].values())[0]} and b values {list(final['B'].values())[0]}")
+        # print(f"A values {list(final['A'].values())[0]} and b values {list(final['B'].values())[0]}")
         if list(final['A'].values())[0] > list(final['B'].values())[0]:
             
             win =True
         else:
             win=False
     elif user_input=='B':
-        print(f"A values {list(final['A'].values())[0]} and b values {list(final['B'].values())[0]}")
+        # print(f"A values {list(final['A'].values())[0]} and b values {list(final['B'].values())[0]}")
         if list(final['A'].values())[0] < list(final['B'].values())[0]:
             
             win =True
@@ -85,55 +91,58 @@ def compare(data,user_input):
 
 # A_B = True
 # print(logo)
-name_A=[key for key in final['A']][0]
-name_B=[key for key in final['B']][0]
-def get_all_data_random_star(data,name):
+# final=generate_two_stars(data)
+# name_A=[key for key in final['A']][0]
+# name_B=[key for key in final['B']][0]
+def get_all_data_random_star(data,name,turn):
+    """
+    This function return the data of user
+    """
     final_dict ={}
     for dict in data:
         for k,v in dict.items():
             if dict['name']==name:
                 final_dict[k]=v
-    return f"Compare A: {name}, a {final_dict['description']}, from country {final_dict['country']}"
+    return f"Compare {turn}: {name}, a {final_dict['description']}, from country {final_dict['country']}"
 
 
-# print(get_all_data_random_star(data,name_A))
-# # print(final)
 
-# count=0
-# if count==0:
-#     print()
-# print(vs)
 
-def game_high_lower(data,name_1,name_2,vs,logo):
+def game_high_lower(data,vs,logo):
+    """
+    This function generate two case firstly then comparing.. then it will count until the condition is true.
+    Exit in case compare return false and print the final score
+    """
+    final=generate_two_stars(data)
+    name_A=[key for key in final['A']][0]
+    name_B=[key for key in final['B']][0]
     count =0
     print(logo)
-    print(get_all_data_random_star(data,name_1))
+    print(get_all_data_random_star(data,name=name_A,turn='A'))
     print(vs)
-    print(get_all_data_random_star(data,name_2))
+    print(get_all_data_random_star(data,name=name_B,turn='B'))
+
     user_input = input("Who has more followers? Type 'A' or 'B' :")
-    comparison_ans =compare(data,user_input)
+    comparison_ans =compare(user_input,final=final)
+    # print(f"Comparison Ans {comparison_ans}")
     while comparison_ans:
             count+=1
+            final = generate_two_stars(data)
+            name_A=[key for key in final['A']][0]
+            name_B=[key for key in final['B']][0]
             print(logo)
             print(f"You're right! Current Score: {count}")
-            print(get_all_data_random_star(data,name_1))
+            print(get_all_data_random_star(data,name=name_A,turn='A'))
             print(vs)
-            print(get_all_data_random_star(data,name_2))
-            user_input = input("Who has more followers? Type 'A' or 'B' :")
+            print(get_all_data_random_star(data,name=name_B,turn='B'))
+            user_input = input("Who has more followers? Type 'A' or 'B' :")            
+            
+    
+            comparison_ans =compare(user_input,final=final)
     print(f"Sorry, that's wrong. Final score: {count}")
 
-    return user_input
-print(game_high_lower(data=data,name_1=name_A,name_2=name_B,vs=vs,logo=logo))
-
-# print(get_all_data_random_star(data,name_B))
-# display : Compare A: Name, a description, from country name
+    # return user_input
+print(game_high_lower(data=data,vs=vs,logo=logo))
 
 
-
-# while comparison_ans:
-#     count+=1
-#     print()
-# print(count)
-
-    
 
