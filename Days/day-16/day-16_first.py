1faf92806821dfd44a6923a5c5294895e29f7c96
kushal863 <kushalskumar14@gmail.com>
2023-02-11 23:30:51 +0530

    oops_started

---
+++ Days/day-16/day-16_first.py
@@ -0,0 +1,21 @@
+"""
+The turtle library in Python is a built-in graphics library
+ that allows you to create and control a turtle object on a canvas.
+   The turtle object can be moved around the canvas,
+     and its movements can be customized using various commands
+       such as forward, backward, left, right, penup, pendown, etc.
+         The turtle library is often used for teaching programming 
+         concepts to beginners, as it provides a fun and interactive way to learn
+           about variables, loops, and conditional statements.
+
+
+"""
+
+import turtle
+import another_module
+
+print(another_module.another_variable)
+
+timmy=turtle.Turtle()
+
+print(timmy)
