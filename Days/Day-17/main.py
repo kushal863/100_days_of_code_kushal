cfba8fe588b60bb9ca4ac70b82667d2373fcb094
kushal863 <kushalskumar14@gmail.com>
2023-03-01 12:29:58 +0530

    Day-17 Started

---
+++ Days/Day-17/main.py
@@ -0,0 +1,70 @@
+# class User:
+#     pass
+
+# user_1 = User()
+
+# print(user_1)
+
+# user_1.id = 'testing_id'
+# user_1.name ='kushal'
+# user_1.last_name = 'saini'
+
+# print(user_1.name)
+# print(user_1.last_name)
+
+# print(user_1)
+
+
+
+# class User:
+
+#     def __init__(self):
+#         print("Init instialized")
+
+# user_1 = User()
+# user_2 = User()
+
+
+# class User:
+
+#     def __init__(self,name,age) :
+#         self.name = name
+#         self.age = age
+
+#     def display(self):
+#         print(f"Hello user name {self.name} and age is {self.age}")
+
+
+# user_1 = User('kushal',34)
+# print(user_1)
+
+# user_1.display()
+
+
+
+
+# class User:
+
+#     def __init__(self,name) :
+#         self.name = name
+
+#     def display(self,age):
+#         print(f"Hello user name {self.name} and age is {age}")
+
+
+# user_1 = User('kushal')
+# print(user_1)
+
+# user_1.display(34)
+
+
+class User:
+
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username = username
+
+user1 = User(101,'kushal')
+print(user1)
+print(user1.username)
+print(user1.id)
