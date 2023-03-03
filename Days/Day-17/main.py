a11d04327354e1d8a088f3e10865ae00fd7030fd
kushal863 <kushalskumar14@gmail.com>
2023-03-03 12:07:21 +0530

    oops understanding done

--- Days/Day-17/main.py
+++ Days/Day-17/main.py
@@ -78,24 +78,63 @@
 # print(user2.id)
 
 
+# class User:
+
+#     def __init__(self,user_id,username):
+#         self.id = user_id
+#         self.username = username
+#         self.follower = 0
+
+
+
+# user1 = User(101,'kushal')
+# user2 = User(102,'ricky')
+
+# # print(user1)
+# print(user1.username)
+# print(user1.id)
+# print(user1.follower)
+
+# # print(user2)
+# print(user2.username)
+# print(user2.id)
+# user2.follower = 34
+# print(user2.follower)
+
+
+
 class User:
 
     def __init__(self,user_id,username):
         self.id = user_id
         self.username = username
         self.follower = 0
+        self.following =0
+
+
+    def follow(self,user):
+        user.follower +=1
+        self.following+=1
 
 
 
 user1 = User(101,'kushal')
 user2 = User(102,'ricky')
 
-# print(user1)
-print(user1.username)
-print(user1.id)
-print(user1.follower)
+user1.follow(user2)
 
-# print(user2)
-print(user2.username)
-print(user2.id)
+# print(user1)
+# print(user1.username)
+# print(user1.id)
+# print(user1.follower)
+
+# # print(user2)
+# print(user2.username)
+# print(user2.id)
+# user2.follower = 34
+# print(user2.follower)
+
+print(user1.follower)
+print(user1.following)
 print(user2.follower)
+print(user2.following)
