7873e7cc70a918dac7d6b5254a1fd2a97f7b2189
kushal863 <kushalskumar14@gmail.com>
2023-03-13 11:58:14 +0530

    Day-17 Completed

--- Days/Day-17/project_quiz/quiz_brain.py
+++ Days/Day-17/project_quiz/quiz_brain.py
@@ -8,6 +8,7 @@ class QuizBrain:
     def __init__(self,questions_list):
         self.questions_list = questions_list
         self.questions_number =0
+        self.user_score =0
 
     def still_has_questions(self):
         question_num = self.questions_number
@@ -20,10 +21,23 @@ class QuizBrain:
 
     def next_question(self):
 
-        current_question =self.questions_list[self.questions_number].question
+        current_question =self.questions_list[self.questions_number]
         self.questions_number+=1
         
-        question = f"Q.{self.questions_number}: {current_question} (True/False)?:"
+        question = f"Q.{self.questions_number}: {current_question.question} (True/False)?:"
         # item = self.questions_list[self.questions_number]
-        user_input=input(question)
+        user_answer=user_input=input(question)
+        self.check_answer(user_answer,current_question.answer)
+
+
+    def check_answer(self,user_answer,correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.user_score +=1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.user_score}/{self.questions_number}")
+        print("\n")
+            
         
