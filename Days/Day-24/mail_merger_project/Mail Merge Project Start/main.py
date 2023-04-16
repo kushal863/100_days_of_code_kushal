1d652f9759f78cb4bc8154f2a109b9259efb9640
kushal863 <kushalskumar14@gmail.com>
2023-04-16 15:13:59 +0530

    mail_merger

--- Days/Day-24/mail_merger_project/Mail Merge Project Start/main.py
+++ Days/Day-24/mail_merger_project/Mail Merge Project Start/main.py
@@ -8,28 +8,46 @@
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
-
+PLACEHOLDER = '[name]'
 # get this letter content
-
-with open("Input\Letters\starting_letter.txt", mode="r") as data:
-
-    letter_content = data.readlines()
-
 # Reading recipient names
 with open(r"Input\Names\invited_names.txt", mode="r") as names:
     invited_names = names.readlines()
 
 
-for name in invited_names:
-    name = f"{name.strip()},"
-    # namee = f"{name},"
-    # print(name)
-    sender_name = ' '.join(letter_content[0].split()[1:])
-    # print(sender_name)
-    # sender_name=sender_name.strip('')
-    letter_content[0]=letter_content[0].replace(sender_name,name)
-    with open(f"Output\ReadyToSend\letter_for_{name[:-1]}.txt",mode="w") as sender_write:
-        sender_write.writelines(letter_content)
+
+
+with open("Input\Letters\starting_letter.txt", mode="r") as data:
+    letter_content = data.read()
+
+    for name in invited_names:
+        stripped_name = name.strip()
+        new_letter=letter_content.replace(PLACEHOLDER,stripped_name)
+
+        with open(f"Output\ReadyToSend\letter_for_{stripped_name}.txt",mode="w") as sender_write:
+            sender_write.writelines(new_letter)
+
+        # print(new_letter)
+
+
+
+
+
+
+    
+
+
+
+# for name in invited_names:
+#     name = f"{name.strip()},"
+#     # namee = f"{name},"
+#     # print(name)
+#     sender_name = ' '.join(letter_content[0].split()[1:])
+#     # print(sender_name)
+#     # sender_name=sender_name.strip('')
+#     letter_content[0]=letter_content[0].replace(sender_name,name)
+#     with open(f"Output\ReadyToSend\letter_for_{name[:-1]}.txt",mode="w") as sender_write:
+#         sender_write.writelines(letter_content)
     
 
 
