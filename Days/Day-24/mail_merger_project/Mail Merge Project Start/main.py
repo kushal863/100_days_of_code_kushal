c47acdbb4c549da6cea0c114a20083741d4fa75b
kushal863 <kushalskumar14@gmail.com>
2023-04-16 14:09:09 +0530

    mail_merger

--- Days/Day-24/mail_merger_project/Mail Merge Project Start/main.py
+++ Days/Day-24/mail_merger_project/Mail Merge Project Start/main.py
@@ -6,3 +6,30 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+
+# get this letter content
+
+with open("Input\Letters\starting_letter.txt", mode="r") as data:
+
+    letter_content = data.readlines()
+
+# Reading recipient names
+with open(r"Input\Names\invited_names.txt", mode="r") as names:
+    invited_names = names.readlines()
+
+
+for name in invited_names:
+    name = f"{name.strip()},"
+    # namee = f"{name},"
+    # print(name)
+    sender_name = ' '.join(letter_content[0].split()[1:])
+    # print(sender_name)
+    # sender_name=sender_name.strip('')
+    letter_content[0]=letter_content[0].replace(sender_name,name)
+    with open(f"Output\ReadyToSend\letter_for_{name[:-1]}.txt",mode="w") as sender_write:
+        sender_write.writelines(letter_content)
+    
+
+
