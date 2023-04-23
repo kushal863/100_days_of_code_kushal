837bc376811457708f6cfbe821718b24ac1bfada
kushal863 <kushalskumar14@gmail.com>
2023-04-23 17:51:55 +0530

    7 questions completed

--- Days/Day-24/python1test.py
+++ Days/Day-24/python1test.py
@@ -1,8 +1,7 @@
 import math
 import pandas as pd
 import itertools
-import pandas as pd
-import numpy as pd
+import numpy as np
 """
 1) Write a python script to determine odd  number from given a sequence of numbers?
 Answer: Input => [2, 3, 4, 5] => Output => [False, True, False, True]
@@ -84,7 +83,7 @@ about ties for now. (ignore the case)
 
 input = ["amit", "gajanan", "ahmed","kushal" ,'sd']
 
-print(sorted(input,key=len))
+# print(sorted(input,key=len))
 
 # Another Solution using bubble sort algorithm
 
@@ -92,5 +91,62 @@ for i in range(len(input)):
     for j in range(len(input)-i-1):
         if len(input[j]) > len(input[j+1]):
             input[j], input[j+1] = input[j+1], input[j]
-    print(f"outer loop {i}:  {input}")
+    
 # print(input)
+
+
+
+"""
+5) Given two dictionary: d1 = {"a":  5, "b" : 6}, d2 = {"c" : 10}, merge them to create a new dictionary
+Answer : Output => {"a" : 5, "b" : 6, "c": 10}
+Input => d1 = {"a" : 10, "k" : 11}, d2 = {"b" : 12, "c" : 16}
+Output => {"a": 10, "k" : 11, "b" : 12, "c" : 16}
+
+"""
+d1 = {"a" : 10, "k" : 11}
+d2 = {"b" : 12, "c" : 16}
+new_dic ={}
+
+new_dic.update(d1)
+new_dic.update(d2)
+# print(new_dic)
+
+"""
+6) what are the methods present in list and tuples in python,can you write a code using list comprehension or for loop to determine which or any other method
+
+which method are exclusively present in list but not present in tuples
+
+"""
+
+
+list_methods=dir(list)
+
+tuple_methods = dir(tuple)
+
+listt =[]
+for m in list_methods:
+    if m not in tuple_methods:
+        listt.append(m)
+
+# print(set(listt))
+
+
+
+
+"""
+7) Given a list of string which contains Name and age [ "AmitKumar34", "RavindraSingh45", "RashmiPrasad10" ]
+Split them such that name and age should be present in a tuple
+Answer: [[('AmitKumar', '34')], [('RavindraSingh', '45')], [('RashmiPrasad', '10')]]
+
+
+"""
+
+input=[ "AmitKumar34", "RavindraSingh45", "RashmiPrasad10" ]
+sol = []
+for i in input:
+    name = i[:-2]
+    age = i[-2:]
+    sol.append([(name,age)])
+
+print(sol)
+
