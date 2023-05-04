3dd76c212448a09dad4134452d9db1d82d49466a
kushal863 <kushalskumar14@gmail.com>
2023-05-04 22:52:38 +0530

    class

--- Days/Day-24/pythontest2.py
+++ Days/Day-24/pythontest2.py
@@ -136,3 +136,18 @@ class Rectangle:
     def __init__(self,length,width) -> None:
         self.length = length
         self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return  2*(self.length* self.width)
+    
+class Square(Rectangle):
+
+    def __init__(self, length, width) -> None:
+        super().__init__(length, width)
+
+    
+
+    
