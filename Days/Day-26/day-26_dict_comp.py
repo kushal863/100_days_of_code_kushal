0fce6fd4ad59806123f5f5ad7f3647f02a3e1aa5
kushal863 <kushalskumar14@gmail.com>
2023-05-27 23:10:55 +0530

    Day-26 Completed

---
+++ Days/Day-26/day-26_dict_comp.py
@@ -0,0 +1,17 @@
+# Distionary Comprehention
+
+#new_dict = {new_key:new_value for item in list or dict}
+
+# this is the way to shorter the code
+
+# Conditional Disctionary Comprehention
+
+#new__dict = {new_dict:value for (key,value) in dict.items() if test}
+
+# STUDENT_SCORE ={'Alex': 74, 'Beth': 71, 'Caroline': 91, 'dave': 96, 'Elenaor': 56, 'Freddie': 82}
+
+# PASSED_STUDENT = {name:value for (name,value) in STUDENT_SCORE.items() if value >60}
+
+# print(PASSED_STUDENT)
+
+
